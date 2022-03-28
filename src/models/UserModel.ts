021019fb0c2fc681b1730228eff1a7b52a96f658
mohammad-fazeli <moohammadfazeli@gmail.com>
2022-03-28 14:49:57 +0430

    create user model

---
+++ src/models/UserModel.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+export interface UserDocument extends mongoose.Document {
+  name: string;
+  email: string;
+  password: string;
+  isAdmin: boolean;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: { type: Boolean, required: true, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model<UserDocument>("User", UserSchema);
+
+export default User;
